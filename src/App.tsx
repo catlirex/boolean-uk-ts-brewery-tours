d849084c17d111450d011968acd7d2fac9303f83
Catherine Li <catherine.uk.20@gmail.com>
2021-07-15 11:07:19 +0100

    updated Brewery type

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,15 @@ import ListSection from "./components/ListSection";
 
 export type Brewery = {
   id: number;
-  obdb_id: string;
   name: string;
   brewery_type: string;
   street: string;
-  address_2: string | null;
-  address_3: string | null;
   city: string;
   state: string;
-  county_province: string | null;
   postal_code: string;
   country: string;
-  longitude: null;
-  latitude: null;
   phone: string;
   website_url: string;
-  updated_at: string;
-  created_at: string;
 };
 
 export type FilterType = "micro" | "regional" | "brewpub" | "";
